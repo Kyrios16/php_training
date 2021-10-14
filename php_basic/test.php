d56ea5a76df6bd7219462d1d25ec2ecbf0898d46
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-14 16:52:13 +0630

    testing php basic

---
+++ php_basic/test.php
@@ -0,0 +1,25 @@
+<?php 
+ 
+ /*
+ *show student information
+ *@param $name
+ *@param $rollNumber
+ */
+ class StudentInfo {
+     public $name;
+     public $rollNumber;
+     public function __construct($name, $rollNumber) {
+         $this->name = $name;
+         $this->rollNumber = $rollNumber;
+     }
+
+     public function showInfo() {
+        return 'Student name is ' .$this->name. ' and roll number is ' .$this->rollNumber;
+     } 
+ }
+
+ $newStudent = new StudentInfo('Kaung Khant Naing', '5CS-18');
+ echo $newStudent->showInfo();
+ echo '<br>';
+ $newStudent = new StudentInfo('Kaung Kaung', '5CS-17');
+ echo $newStudent->showInfo();
