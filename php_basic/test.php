b2de2940ab825b11285574ad6848cf5099b99464
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-15 09:07:33 +0630

    testing function commit

--- php_basic/test.php
+++ php_basic/test.php
@@ -1,5 +1,4 @@
 <?php 
- 
  /*
  *show student information
  *@param $name
@@ -23,3 +22,17 @@
  echo '<br>';
  $newStudent = new StudentInfo('Kaung Kaung', '5CS-17');
  echo $newStudent->showInfo();
+ echo '<br>';
+ echo '<br>';
+
+ $x =0;
+ while ($x < 10) {
+     if($x == 4) {
+         $x++;
+         continue;
+     }
+     echo 'number is ' .$x.'<br>';
+     $x++;
+ }
+
+ 
