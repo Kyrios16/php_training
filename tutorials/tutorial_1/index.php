d6ebb136044c13c03d4ce053bcf1b9c97bf143e9
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-15 16:48:47 +0630

    review fix for tutorial_1

--- tutorials/tutorial_1/index.php
+++ tutorials/tutorial_1/index.php
@@ -5,22 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutorial_1</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <table>
     <?php for ($row=1; $row<=8; $row++) { ?>
         <tr>
-    <?php for ($col=1; $col<=8; $col++) {
+        <?php for ($col=1; $col<=8; $col++) {
             $total= $row + $col;
-            if($total%2 == 0) { ?>
-                <td class='white-cell'></td>
+            if($total%2 == 0) { 
+        ?>
+            <td class='white-cell'></td>
             <?php } else { ?>
                 <td class='black-cell'></td>
             <?php } ?>
-            <?php } ?>
+        <?php } ?>
         </tr>
-     <?php } ?>
+    <?php } ?>
     </table>
 </body>
 </html>
