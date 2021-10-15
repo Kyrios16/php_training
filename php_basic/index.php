37a1a460126059d56113bf351d59434c356a3a4f
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-15 09:13:15 +0630

    create index.php for testing request

---
+++ php_basic/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>PHP Request</title>
+</head>
+<body>
+ <h1>PHP Request</h1>
+ <form action="test.php" method="get">
+     <label for="name">Name</label>
+     <input type="text" placeholder="Name">
+     <label for="email">Email</label>
+     <input type="email" placeholder="Email">
+  <button type="submit">Send Data</button>
+ </form>
+</body>
+</html>
