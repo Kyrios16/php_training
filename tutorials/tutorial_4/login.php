9ebbc3f5d6060c3cea5181842e7b989e88472aaf
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-18 15:38:50 +0630

    add logout.php file commit

--- tutorials/tutorial_4/login.php
+++ tutorials/tutorial_4/login.php
@@ -15,6 +15,7 @@
         if (isset($usersAcc[$username]) && $usersAcc[$username] == $password){
             $_SESSION['userData']['username'] = $usersAcc[$username];
             echo "<h2 style='text-align:center'> Login Successfully </h2>";
+            echo "<a href='logout.php'>Logout</a>";
             exit;
         } else {
             header("location: index.php?login=failed");
