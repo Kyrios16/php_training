ab62fc77d0111a7f7d1bfb2c68d964bf80eb120f
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-18 14:20:06 +0630

    add tutorial-4 commit

---
+++ tutorials/tutorial_4/index.php
@@ -0,0 +1,42 @@
+<?php include 'login.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutorial-5</title>
+  <link rel="stylesheet" href="library/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="wrap">
+
+    <?php if( isset($_GET['login']) && $_GET['login'] == "failed"): ?>
+        <div class="alert alert-danger">
+            Login failed! User name or password incorrect.
+        </div>
+    <?php endif ?>
+
+    <h3 class="mb-3 text-muted">Login</h3>
+    <form action=" " method="post">
+        <input
+            type="text" 
+            name="username"
+            id="username"
+            class="form-control mb-3"
+            placeholder="Username" required
+        >
+        <input
+            type="password" 
+            name="password"
+            id="password"
+            class="form-control mb-3"
+            placeholder="Password" required
+        >
+        <input class="btn btn-primary" type="submit" name="submit" value="Login">
+    </form>   
+  </div>
+  <!-- /.wrap -->
+</body>
+</html>
---
+++ tutorials/tutorial_4/login.php
@@ -0,0 +1,23 @@
+<?php 
+    session_start();
+
+    /*check login validate*/
+    if(isset($_POST['submit'])){
+        $usersAcc = array(
+            "john" => "123456",
+            "bob" => "214365",
+            "mary" => "654321"
+        );
+        $username = isset($_POST['username']) ? $_POST['username'] : '';
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+        /* Check Username and password */
+        if (isset($usersAcc[$username]) && $usersAcc[$username] == $password){
+            $_SESSION['userData']['username'] = $usersAcc[$username];
+            echo "<h2 style='text-align:center'> Login Successfully </h2>";
+            exit;
+        } else {
+            header("location: index.php?login=failed");
+        }
+    }
+?>
