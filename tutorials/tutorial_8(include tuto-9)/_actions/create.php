4714cadca55d836a0a027f2c01f810d80f8193cf
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-21 14:21:24 +0630

    fix error commit

--- tutorials/tutorial_8(include tuto-9)/_actions/create.php
+++ tutorials/tutorial_8(include tuto-9)/_actions/create.php
@@ -6,10 +6,11 @@ $email = $_POST['email'];
 $job = $_POST['job'];
 $phone = $_POST['phone'];
 $address = $_POST['address'];
+$salary = $_POST['salary'];
 $password = $_POST['password'];
 
-$sql = "INSERT INTO users (username, email, job, phone, address, password, created_at, updated_at) 
-		VALUES ('$username',' $email', '$job', $phone, '$address', '$password', now(), now())";
+$sql = "INSERT INTO users (username, email, job, phone, address, salary, password, created_at, updated_at) 
+		VALUES ('$username',' $email', '$job', $phone, '$address', '$salary', '$password', now(), now())";
 
 mysqli_query($conn, $sql);
 header("location: ../index.php");
