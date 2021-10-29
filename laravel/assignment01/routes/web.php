ace8e91e42a6deb95cf4dfd438b6c334dadd6f32
Kyrios16 <kaungkhantnaing168@gmail.com>
2021-10-29 15:45:27 +0630

    change directory

--- laravel/assignment01/routes/web.php
+++ laravel/assignment01/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,16 +13,19 @@
 |
 */
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
+
+Route::get('/posts', [PostController::class, 'index'])->name('index');
+Route::post('/posts', [PostController::class, 'store'])->name('post.save');
+Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
 Route::put('/posts/{post}', [PostController::class, 'update']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy']);
